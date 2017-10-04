16ea3fa9387e4ca07dfa2a3026a66f74cd33cc38
Jameson Knoll <j.knoll621@gmail.com>
2017-10-04 18:36:40 +0800

    create file directory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import store, { history } from './store'
+import App from './app/components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './styles/index.css'
+
+const target = document.querySelector('#root')
+
+/*history.listen((location, action) => {
+  window.scrollTo(0, 0);
+})
+*/
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history} >
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
+)
